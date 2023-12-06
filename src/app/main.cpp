d54e04a330ae8e28ab05b95851e71874e45c0bc2
Poet <602289550@qq.com>
2023-12-06 14:33:32 +0800

    updated

--- src/app/main.cpp
+++ src/app/main.cpp
@@ -8,7 +8,7 @@ using namespace mscut;
 
 using ScalarFunc = core::MSCuttingModel::ScalarFunc;
 
-const std::string funcName = "yuanzhui";
+const std::string funcName = "torus";
 // level-set定义: 函数式和梯度
 static Scalar val(const Eigen::Vector3d& p)
 {
@@ -23,7 +23,26 @@ static Scalar val(const Eigen::Vector3d& p)
 	double x = p.x();
 	double y = p.y();
 	double z = p.z();
-	return std::pow(x * x + z * z, 0.5) - std::pow(y * y, 0.5);
+	//return std::pow(x * x + z * z, 0.5) - std::pow(y * y, 0.5);							//yuanzhuixian1
+	//return std::pow(x * x + y * y, 0.5) - std::pow(z * z, 0.5);							//yuanzhuixian2
+	//return std::pow((std::pow(8 * x * x + 8 * z * z, 0.5) - 2), 2) + 8 * y * y - 2;		//torus1
+	//return std::pow((std::pow(8 * x * x + 8 * y * y, 0.5) - 2), 2) + 8 * z * z - 2;		//torus2
+	//return std::pow((x / 2), 4) + std::pow((y / 2), 4) + std::pow((z / 2), 4) - 0.03;		//tuoqiuti
+	//return std::pow(4 * x * x + 4 * y * y + 4 * z * z, 2) - 3 * (4 * x * x - 4 * y * y) - 0.8;		//xingqiuti1
+	//return std::pow(4 * x * x + 4 * y * y + 4 * z * z, 2) - 3 * (4 * y * y - 4 * z * z) - 0.8;		//xingqiuti2
+	//return (15 * x * x + 15 * y * y + 15 * z * z + 1) * (15 * x * x + 15 * y * y + 15 * z * z + 1) - 16 * (15 * x * x + 15 * y * y) - 1;		//jiaoChaHuanMian1
+	//return std::pow((15 * x * x + 15 * y * y + 15 * z * z + 1), 2) - 16 * (15 * y * y + 15 * z * z) - 1;		//jiaoChaHuanMian2
+	//return std::pow(x * x + y * y + z * z - 1, 2) - z * z - 2 * x - 1.5; // penut
+
+	return (4 * x * x + 4 * y * y + 4 * z * z + 4 * y - 1) * (std::pow((4 * x * x + 4 * y * y + 4 * z * z - 4 * y - 1), 2) - 32 * z * z) +
+		64 * x * z * (4 * x * x + 4 * y * y + 4 * z * z - 4 * y - 1);  // klein bottle - 适用于hole/bettle/carp_muscle model
+
+	//return std::pow((0.5 * x * x + 0.5 * z * z), 3) - 0.25 * x * x * z * z; // QUATREFOIL CURVE - 适用于hole model
+	//return std::pow((4 * x * x + 4 * y * y), 3) - 16 * x * x * y * y; // QUATREFOIL CURVE - 适用于bettle model
+	//return std::pow((x * x + y * y), 3) - x * x * y * y; // QUATREFOIL CURVE - 适用于carp_muscle model
+
+	//return -2 * y - 4 * x * z + 8 * x * x * y - 16 * x * x * z + 8 * y * y * y - 16 * y * y * z + 16 * y * z * z; // mobius - 适用于hole/bettle model
+	//return -y - 2 * x * z + x * x * y - 2 * x * x * z + y * y * y - 2 * y * y * z + y * z * z; // mobius - 适用于carp_muscle model
 }
 static Eigen::Vector3d grad(const Eigen::Vector3d& p)
 {
@@ -38,7 +57,38 @@ static Eigen::Vector3d grad(const Eigen::Vector3d& p)
 	double x = p.x();
 	double y = p.y();
 	double z = p.z();
-	return Eigen::Vector3d(x / sqrt(x * x + z * z), -y / sqrt(y * y), z / sqrt(x * x + z * z));
+	//return Eigen::Vector3d(x / sqrt(x * x + z * z), -y / sqrt(y * y), z / sqrt(x * x + z * z));	//yuanzhuixian1
+	//return Eigen::Vector3d(x / sqrt(x * x + y * y), -y / sqrt(x * x + y * y), z / sqrt(z * z));	//yuanzhuixian2
+
+	//return Eigen::Vector3d(16 * x * (1 - 1 / std::pow((2 * x * x + 2 * z * z), 0.5)), 16 * y, 16 * z * (1 - 1 / std::pow((2 * x * x + 2 * z * z), 0.5)));		//torus1
+	//return Eigen::Vector3d(8 * x * (2 - sqrt(2) / sqrt(x * x + y * y)), 8 * y * (2 - sqrt(2) / sqrt(x * x + y * y)), 16 * z);		//torus2
+	//return Eigen::Vector3d(x * x * x / 4, y * y * y / 4, z * z * z / 4);															//tuoqiuti
+	//return 8 * Eigen::Vector3d(x * (-3 + 8 * x * x + 8 * y * y + 8 * z * z), y * (3 + 8 * x * x + 8 * y * y + 8 * z * z), 8 * z * (x * x + y * y + z * z));	//xingqiuti1
+	//return 8 * Eigen::Vector3d(8 * x * (x * x + y * y + z * z), y * (-3 + 8 * x * x + 8 * y * y + 8 * z * z), z * (3 + 8 * x * x + 8 * y * y + 8 * z * z));	//xingqiuti2
+	//double ele = 15 * x * x + 15 * y * y + 15 * z * z;
+	//return 60 * Eigen::Vector3d(x * (-7 + ele), y * (-7 + ele), z * (1 + ele));			//jiaoChaHuanMian1
+	//return 60 * Eigen::Vector3d(x * (1 + ele), y * (-7 + ele), z * (-7 + ele));		//jiaoChaHuanMian2
+	//double t = -1 + x * x + y * y + z * z;
+	//return Eigen::Vector3d(-2 + 4 * x * t, 4 * y * t, -2 * z + 4 * z * t); // peanut
+
+
+	return Eigen::Vector3d(8 * (48 * std::pow(x, 5) + 96 * x * x * z + 8 * z * (-1 - 4 * y + 4 * y * y + 4 * z * z) +
+		8 * std::pow(x, 3) * (-3 - 4 * y + 12 * y * y + 12 * z * z) + x * (3 - 32 * std::pow(y, 3) + 48 * std::pow(y, 4) -
+			56 * z * z + 48 * std::pow(z, 4) + y * (8 - 32 * z * z) + 8 * y * y * (-5 + 12 * z * z))),
+
+		64 * x * (-4 + 8 * y) * z + 8 * (-1 + 2 * y) * (-1 + 4 * x * x - 4 * y + 4 * y * y + 4 * z * z) * (-1 + 4 * x * x + 4 * y + 4 * y * y + 4 * z * z) +
+		(4 + 8 * y) * (-32 * z * z + std::pow(-1 + 4 * x * x - 4 * y + 4 * y * y + 4 * z * z, 2)),
+
+		8 * (64 * x * z * z - 32 * std::pow(z, 3) + 8 * x * (-1 + 4 * x * x - 4 * y + 4 * y * y + 4 * z * z) +
+			z * std::pow((-1 + 4 * x * x - 4 * y + 4 * y * y + 4 * z * z), 2) +
+			2 * z * (-5 + 4 * x * x - 4 * y + 4 * y * y + 4 * z * z) * (-1 + 4 * x * x + 4 * y + 4 * y * y + 4 * z * z))); // klein bottle - 适用于hole/bettle/carp_muscle model
+
+	//return Eigen::Vector3d(-0.5 * x * z * z + 0.75 * x * std::pow(x * x + z * z, 2), 0, -0.5 * x * x * z + 0.75 * z * std::pow(x * x + z * z, 2)); // QUATREFOIL CURVE - 适用于hole model
+	//return Eigen::Vector3d(8 * x * (-4 * y * y + 48 * std::pow(x * x + y * y, 2), 8 * y * (-4 * x * x + 48 * std::pow(x * x + y * y, 2), 0))); // QUATREFOIL CURVE - 适用于bettle model
+	//return Eigen::Vector3d(-2 * x * y * y + 6 * x * std::pow(x * x + y * y, 2), -2 * x * x * y + 6 * y * std::pow(x * x + y * y, 2), 0);  // QUATREFOIL CURVE - 适用于carp_muscle model
+
+	//return Eigen::Vector3d(16 * x * (y - 2 * z) - 4 * z, -2 + 8 * x * x + 24 * y * y - 32 * y * z + 16 * z * z, -4 * (x + 4 * x * x + 4 * y * (y - 2 * z))); // mobius - 适用于hole/bettle model
+	//return Eigen::Vector3d(2 * x * (y - 2 * z) - 2 * z, -1 + x * x + 3 * y * y - 4 * y * z + z * z, -2 * (x + x * x + y * (y - z))); // mobius - 适用于carp_muscle model
 }
 
 int main(int argc, char** argv)
@@ -98,7 +148,7 @@ int main(int argc, char** argv)
 	std::ofstream mae_out(mae_vis_file);
 	mae_out << funcName << "\n";
 
-	for (int i = 0; i <= iter; ++i)
+	for (int i = 20; i <= iter; ++i)
 	{
 		const std::string pd_vis_file = str_util::concatFilePath(
 			meshNormVisDir, std::to_string(numSamplePoints), funcName, "circle", "isoline_" + std::to_string(i) + ".obj"
@@ -127,4 +177,4 @@ int main(int argc, char** argv)
 	unit_test::testComputeCDT();*/
 
 	return 0;
-}
+	}
